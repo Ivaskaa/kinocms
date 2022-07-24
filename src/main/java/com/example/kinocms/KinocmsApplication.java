95bb51276a4bdd80962db8f3b4fbcc12b2f34269
Ivaskaa <ivansha200@gmail.com>
2022-07-24 14:28:11 +0300

    commit

--- src/main/java/com/example/kinocms/KinocmsApplication.java
+++ src/main/java/com/example/kinocms/KinocmsApplication.java
@@ -1,12 +1,11 @@
 package com.example.kinocms;
 
 //import com.example.kinocms.service.MailSender;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.boot.security.servlet.ApplicationContextRequestMatcher;
 import org.springframework.context.event.EventListener;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
